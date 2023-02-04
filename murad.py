725c22f0e0efe656f82ee65f692344faa5b01cd8
tester214pro <123368658+tester214pro@users.noreply.github.com>
2023-02-04 15:15:42 +0000

    Pending changes exported from your codespace

--- murad.py
+++ murad.py
@@ -1,8 +1,16 @@
-TOKEN = "5955466488:AAHgKxmA1OzaDO7N63qAgbRW7TQ4r9Iht9o" 
+TOKEN = "5955466488:AAHgKxmA1OzaDO7N63qAgbRW7TQ4r9Iht9o"  # ТОКЕН для GIT-TES BOT
+# Приветствие при нажатии команды старт.
+PRIVET = "Привет это пока тестовый бот !!!"
+HELP = "Скоро тут будет мини инструкция!!"  # Нажатие команды помощь.
 
-@bot.message_handler(commands=['start'])
-def start_message(message):
-    keyboard = telebot.types.ReplyKeyboardMarkup(True)
-    keyboard.row('❤Наш Инстаграм')
-    keyboard.row('Заказать Нож')
-    bot.send_message(message.chat.id, 'Привет Выберите кнопку!', reply_markup=keyboard)
+PRAICE13 = "🔥  iPhone 13 🔥\n13 128 white 49500 IN(замята коробка)\n13 128 white 51500 IN\n13 128 pink 51500 IN\n13 128 green 51500 IN\n13 128 blue 49700 IN\n13 256 blue 59000 IN (замята коробка)\n13 PRO 128 gold 83000 LL\n13 PRO 128 black 83000 LL\n13 PRO 256 black 89000 LL"
+
+
+
+
+IPHONE14 = "iPhone 14"
+IPHONE14P = "14PRO"
+IPHONE14PMAX = "14PRO MAX"
+IPHONE13 = "iPhone 13"
+IPHONE13P = "13PRO"
+IPHONE13PMAX = "13PRO MAX"
